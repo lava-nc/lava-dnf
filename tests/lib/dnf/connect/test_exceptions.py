534348e367ec131a11406b46de47a4243ef7ce8a
Mathis Richter <mathis.richter@intel.com>
2021-11-04 18:43:05 +0100

    Added exception for misconfigured operations
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>

--- tests/lib/dnf/connect/test_exceptions.py
+++ tests/lib/dnf/connect/test_exceptions.py
@@ -5,6 +5,7 @@
 import unittest
 
 from lava.lib.dnf.connect.exceptions import MissingOpError, DuplicateOpError
+from lava.lib.dnf.operations.exceptions import MisconfiguredOpError
 
 
 class TestMissingOpError(unittest.TestCase):
@@ -37,6 +38,17 @@ class TestDuplicateOpError(unittest.TestCase):
 
         # check whether the default message is set
         self.assertTrue(context.exception.args[0] is not None)
+
+
+class TestMisconfiguredOpError(unittest.TestCase):
+    def test_raising_misconfigured_op_error(self):
+        """Tests whether the MisconfiguredOpError can be raised."""
+        msg = "test message"
+        with self.assertRaises(MisconfiguredOpError) as context:
+            raise MisconfiguredOpError(msg)
+
+        # check whether the message is set
+        self.assertEqual(context.exception.args[0], msg)
 
 
 if __name__ == '__main__':

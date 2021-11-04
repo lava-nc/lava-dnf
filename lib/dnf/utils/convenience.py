08051ddbbc012b2bd9641e1179e9689397547532
Mathis Richter <mathis.richter@intel.com>
2021-11-04 12:35:36 +0100

    Added num_dims function
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>

--- lib/dnf/utils/convenience.py
+++ lib/dnf/utils/convenience.py
@@ -21,3 +21,23 @@ def num_neurons(shape: ty.Tuple[int, ...]) -> int:
         number of neurons
     """
     return int(np.prod(shape))
+
+
+def num_dims(shape: ty.Tuple[int, ...]) -> int:
+    """
+    Computes the dimensionality of a shape, assuming that (1,) represents
+    a zero-dimensional shape.
+
+    Parameters
+    ----------
+    shape : tuple(int)
+        shape of a population of neurons
+
+    Returns
+    -------
+    number of dimensions : int
+    """
+    # assume dimensionality 0 if there is only a single neuron
+    dims = 0 if num_neurons(shape) == 1 else len(shape)
+
+    return dims
--- tests/lib/dnf/utils/test_convenience.py
+++ tests/lib/dnf/utils/test_convenience.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from lib.dnf.utils.convenience import num_neurons
+from lib.dnf.utils.convenience import num_neurons, num_dims
 
 
 class TestNumNeurons(unittest.TestCase):
@@ -20,6 +20,25 @@ class TestNumNeurons(unittest.TestCase):
         num = num_neurons(shape=(5, 3))
         self.assertEqual(num, 15)
 
+
+class TestNumDims(unittest.TestCase):
+    def test_num_dims_0d(self):
+        """Tests whether dimensionality is computed correctly for
+        zero-dimensional shapes."""
+        dims = num_dims(shape=(1,))
+        self.assertEqual(dims, 0)
+
+    def test_num_neurons_1d(self):
+        """Tests whether dimensionality is computed correctly for
+        one-dimensional shapes."""
+        dims = num_dims(shape=(10,))
+        self.assertEqual(dims, 1)
+
+    def test_num_neurons_2d(self):
+        """Tests whether dimensionality is computed correctly for
+        two-dimensional shapes."""
+        dims = num_dims(shape=(10, 10))
+        self.assertEqual(dims, 2)
 
 if __name__ == '__main__':
     unittest.main()

daaae0775690187167df814d72880f06d44a7862
Mathis Richter <mathis.richter@intel.com>
2021-11-18 18:58:23 +0100

    Added gauss() function

--- src/lava/lib/dnf/utils/math.py
+++ src/lava/lib/dnf/utils/math.py
@@ -2,6 +2,106 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
 
+import numpy as np
+import typing as ty
+import scipy.stats
+
+
+def gauss(shape: ty.Tuple[int, ...],
+          domain: ty.Optional[np.ndarray] = None,
+          amplitude: ty.Optional[float] = 1.0,
+          mean: ty.Optional[ty.Union[float, np.ndarray]] = None,
+          stddev: ty.Optional[ty.Union[float, np.ndarray]] = None):
+    """
+    Evaluates the Gaussian function over a specified domain in multiple
+    dimensions.
+
+    If a domain is specified, the function will evaluate the Gaussian at
+    linearly interpolated values between the lower and upper bounds of the
+    domain. The number of samples is determined by the shape parameter. For
+    example, for given parameters shape=(5,) and domain=[[-5, -1]], it will
+    evaluate the function at positions -5,-4,-3,-2,-1.
+
+    If no domain is specified, the function will evaluate the Gaussian at the
+    indices of the sampling points. For instance, for a given shape of
+    shape=(5,), it will evaluate the function at positions 0,1,2,3,4.
+
+    Parameters
+    ----------
+    shape : tuple(int)
+        number of sampling points along each dimension
+    domain : numpy.ndarray, optional
+        lower and upper bound of input values for each dimension at which
+        the Gaussian function is evaluated
+    amplitude : float, optional
+        amplitude of the Gaussian, defaults to 1
+    mean : numpy.ndarray, optional
+        mean of the Gaussian, defaults to 0
+    stddev : numpy.ndarray, optional
+        standard deviation of the Gaussian, defaults to 1
+
+    Returns
+    -------
+    gaussian : numpy.ndarray
+        multi-dimensional array with samples of the Gaussian
+    """
+    # dimensionality of the Gaussian
+    dimensionality = len(shape)
+
+    # domain defaults to the indices of the sampling points
+    if domain is None:
+        domain = np.zeros((dimensionality, 2))
+        domain[:, 1] = np.array(shape[:]) - 1
+    else:
+        if isinstance(domain, np.ndarray) and domain.shape != (len(shape), 2):
+            raise ValueError("the shape of <domain> is incompatible with "
+                             "the specified <shape>; <domain> should be of "
+                             f"shape ({len(shape)}, 2) but is {domain.shape}")
+
+    # mean defaults to 0
+    if mean is None:
+        mean = np.zeros((dimensionality,))
+    else:
+        if isinstance(mean, np.ndarray) and mean.shape != shape:
+            raise ValueError("the shape of <mean> is incompatible with "
+                             "the specified <shape>; <mean> should be of "
+                             f"shape ({len(shape)},) but is {mean.shape}")
+
+    # standard deviation defaults to 1
+    if stddev is None:
+        stddev = np.ones((dimensionality,))
+    else:
+        if isinstance(stddev, np.ndarray) and stddev.shape != shape:
+            raise ValueError("the shape of <stddev> is incompatible with "
+                             "the specified <shape>; <stddev> should be of "
+                             f"shape ({len(shape)},) but is {stddev.shape}")
+
+    # create linear spaces for each dimension
+    linspaces = []
+    for i in range(dimensionality):
+        linspaces.append(np.linspace(domain[i, 0], domain[i, 1], shape[i]))
+
+    # arrange linear spaces into a meshgrid
+    linspaces = np.array(linspaces, dtype=object)
+    grid = np.meshgrid(*linspaces, copy=False)
+    grid = np.array(grid)
+
+    # swap axes to get around perculiarity of meshgrid
+    if dimensionality > 1:
+        grid = np.swapaxes(grid, 1, 2)
+
+    # reshape meshgrid to fit multi-variate normal
+    grid = np.moveaxis(grid, 0, -1)
+
+    # compute normal probability density function
+    mv_normal_pdf = scipy.stats.multivariate_normal.pdf(grid,
+                                                        mean=mean,
+                                                        cov=stddev)
+    # normalize probability density function and apply amplitude
+    gaussian = amplitude * mv_normal_pdf / np.max(mv_normal_pdf)
+
+    return gaussian
+
 
 def is_odd(n):
     """
--- tests/lava/lib/dnf/unit/utils/test_math.py
+++ tests/lava/lib/dnf/unit/utils/test_math.py
@@ -3,8 +3,86 @@
 # See: https://spdx.org/licenses/
 
 import unittest
+import numpy as np
 
-from lava.lib.dnf.utils.math import is_odd
+from lava.lib.dnf.utils.math import is_odd, gauss
+
+
+class TestGauss(unittest.TestCase):
+    def test_shape(self):
+        """Tests whether the returned Gaussian has the specified shape."""
+        shape = (5, 3)
+        gaussian = gauss(shape)
+        self.assertEqual(gaussian.shape, shape)
+
+    def test_default_values(self):
+        """Tests whether the default values for domain, amplitude, mean, and
+        stddev are used."""
+        shape = (3, 3)
+        gaussian = gauss(shape)
+
+        # amplitude should be 1, domain should be such that
+        # the maximum is at position (0, 0), and mean should be 0
+        self.assertEqual(gaussian[0, 0], 1)
+        # stddev should be symmetrical
+        self.assertEqual(gaussian[0, 1], gaussian[1, 0])
+        self.assertEqual(gaussian[0, 2], gaussian[2, 0])
+        self.assertEqual(gaussian[1, 2], gaussian[2, 1])
+
+    def test_setting_amplitude(self):
+        """Tests whether the amplitude is set to the correct value."""
+        shape = (3, 3)
+        amplitude = 42
+        gaussian = gauss(shape, amplitude=amplitude)
+        self.assertEqual(gaussian[0, 0], amplitude)
+
+    def test_setting_domain(self):
+        """Tests whether the domain is set correctly."""
+        shape = (5,)
+        domain = np.array([[-2.5, 2.5]])
+        gaussian = gauss(shape, domain=domain)
+        # with the specified domain, the maximum (1) should be in the center
+        self.assertEqual(gaussian[2], 1)
+        # and the gaussian should be symmetrical
+        self.assertTrue(np.array_equal(gaussian, np.flip(gaussian)))
+
+    def test_setting_mean(self):
+        """Tests whether the mean is set correctly."""
+        shape = (5,)
+        mean = 1
+        gaussian = gauss(shape, mean=mean)
+        self.assertEqual(gaussian[mean], 1)
+
+    def test_setting_stddev(self):
+        """Tests whether the stddev can be set."""
+        shape = (3,)
+        gaussian_narrow = gauss(shape, stddev=1)
+        gaussian_broad = gauss(shape, stddev=2)
+        self.assertTrue(gaussian_narrow[1] < gaussian_broad[1])
+
+    def test_domain_shape_mismatch_raises_error(self):
+        """Tests whether an error is raised when the shape of the <domain>
+        argument is different from <shape>."""
+        shape = (5, 3)
+        domain = np.zeros((1, 2))  # should be of shape (len(shape), 2)
+        with self.assertRaises(ValueError):
+            gauss(shape, domain=domain)
+
+    def test_mean_shape_mismatch_raises_error(self):
+        """Tests whether an error is raised when the shape of the <mean>
+        argument is different from <shape>."""
+        shape = (5, 3)
+        mean = np.zeros((3,))  # should be of shape (2,)
+        with self.assertRaises(ValueError):
+            gauss(shape, mean=mean)
+
+    def test_stddev_shape_mismatch_raises_error(self):
+        """Tests whether an error is raised when the shape of the <stddev>
+        argument is different from <shape>."""
+        shape = (5, 3)
+        stddev = np.ones((3,))  # should be of shape (2,)
+        with self.assertRaises(ValueError):
+            gauss(shape, stddev=stddev)
 
 
 class TestIsOdd(unittest.TestCase):

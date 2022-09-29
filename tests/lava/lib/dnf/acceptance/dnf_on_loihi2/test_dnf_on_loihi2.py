69fab9658dfbad82318f9b24395f9e69e88e878a
Mathis Richter <mathis.richter@intel.com>
2022-09-30 01:28:43 +0200

    Added unit tests for 2D and 3D DNFs on Loihi 2.
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>

--- tests/lava/lib/dnf/acceptance/dnf_on_loihi2/test_dnf_on_loihi2.py
+++ tests/lava/lib/dnf/acceptance/dnf_on_loihi2/test_dnf_on_loihi2.py
@@ -4,6 +4,7 @@
 
 import unittest
 import numpy as np
+import typing as ty
 import sys
 
 from lava.magma.core.run_configs import Loihi1SimCfg, Loihi2HwCfg
@@ -25,72 +26,65 @@ run_loihi_tests: bool = Utils.get_bool_env_setting("RUN_LOIHI_TESTS")
 class TestDNFOnLoihi2(unittest.TestCase):
     @unittest.skipUnless(run_loihi_tests, 'RUN_LOIHI_TESTS is not enabled.')
     def test_1d_dnf_on_loihi2(self) -> None:
-        num_steps = 10
-        num_neurons = 10
-        shape = (num_neurons,)
+        self._compare_loihi_vs_python(shape=(7,))
 
-        gauss_pattern_params = {"shape": shape,
-                                "amplitude": 3000,
-                                "mean": 5,
-                                "stddev": 3}
-        spike_gen_params = {"shape": shape,
-                            "seed": 1}
-        dnf_params = {"shape": shape,
-                      "du": 409,
-                      "dv": 2047,
-                      "vth": 50}
-        kernel_params = {"amp_exc": 18,
-                         "width_exc": 3,
-                         "global_inh": -15}
+    @unittest.skipUnless(run_loihi_tests, 'RUN_LOIHI_TESTS is not enabled.')
+    def test_2d_dnf_on_loihi2(self) -> None:
+        self._compare_loihi_vs_python(shape=(5, 5))
 
-        ### Python ###
+    @unittest.skipUnless(run_loihi_tests, 'RUN_LOIHI_TESTS is not enabled.')
+    def test_3d_dnf_on_loihi2(self) -> None:
+        self._compare_loihi_vs_python(shape=(3, 3, 3))
 
-        gauss_pattern = GaussPattern(**gauss_pattern_params)
-        spike_generator = RateCodeSpikeGen(**spike_gen_params)
-        dnf = LIF(**dnf_params)
-        kernel = SelectiveKernel(**kernel_params)
-
-        gauss_pattern.a_out.connect(spike_generator.a_in)
-        connect(spike_generator.s_out, dnf.a_in, ops=[Weights(20)])
-        connect(dnf.s_out, dnf.a_in, ops=[Convolution(kernel)])
-
-        voltages_py = np.zeros((num_neurons, num_steps), dtype=int)
-        try:
-            for i in range(num_steps):
-                dnf.run(condition=RunSteps(num_steps=1),
-                        run_cfg=Loihi1SimCfg(select_tag="fixed_pt"))
-                voltages_py[:, i] = dnf.v.get()
-        finally:
-            dnf.stop()
-
-        ### LOIHI 2 ###
-
-        gauss_pattern = GaussPattern(**gauss_pattern_params)
-        spike_generator = RateCodeSpikeGen(**spike_gen_params)
-        injector = PyToNxAdapter(shape=shape)
-        dnf = LIF(**dnf_params)
-
-        gauss_pattern.a_out.connect(spike_generator.a_in)
-        spike_generator.s_out.connect(injector.inp)
-        connect(injector.out, dnf.a_in, ops=[Weights(20)])
-        connect(dnf.s_out, dnf.a_in, ops=[Convolution(kernel)])
-
-        voltages_nc = np.zeros_like(voltages_py)
-        try:
-            for i in range(num_steps):
-                dnf.run(condition=RunSteps(num_steps=1),
-                        run_cfg=Loihi2HwCfg())
-                voltages_nc[:, i] = dnf.v.get()
-        finally:
-            dnf.stop()
-
-        voltages_nc = np.where(voltages_nc == 128, 0, voltages_nc)
+    def _compare_loihi_vs_python(self, shape: ty.Tuple[int, ...]) -> None:
+        voltages_py = self._run_test(shape=shape, run_on_loihi=False)
+        voltages_nc = self._run_test(shape=shape, run_on_loihi=True)
 
         if verbose:
             print(f"{voltages_py=}")
             print(f"{voltages_nc=}")
 
         np.testing.assert_array_equal(voltages_py, voltages_nc)
+
+    @staticmethod
+    def _run_test(shape: ty.Tuple[int, ...], run_on_loihi: bool) -> np.ndarray:
+        num_steps = 10
+        num_dimensions = len(shape)
+
+        gauss_pattern = GaussPattern(shape=shape,
+                                     amplitude=3000,
+                                     mean=[1] * num_dimensions,
+                                     stddev=[2] * num_dimensions)
+        spike_generator = RateCodeSpikeGen(shape=shape, seed=1)
+        dnf = LIF(shape=shape, du=409, dv=2047, vth=50)
+        kernel = SelectiveKernel(amp_exc=18,
+                                 width_exc=[3] * num_dimensions,
+                                 global_inh=-15)
+
+        gauss_pattern.a_out.connect(spike_generator.a_in)
+        if run_on_loihi:
+            injector = PyToNxAdapter(shape=shape)
+            spike_generator.s_out.connect(injector.inp)
+            connect(injector.out, dnf.a_in, ops=[Weights(20)])
+        else:
+            connect(spike_generator.s_out, dnf.a_in, ops=[Weights(20)])
+        connect(dnf.s_out, dnf.a_in, ops=[Convolution(kernel)])
+
+        run_cfg = Loihi2HwCfg() if run_on_loihi else Loihi1SimCfg(
+            select_tag="fixed_pt")
+
+        voltages = np.zeros(shape=shape + (num_steps,), dtype=int)
+        try:
+            for i in range(num_steps):
+                dnf.run(condition=RunSteps(num_steps=1), run_cfg=run_cfg)
+                voltages[..., i] = dnf.v.get()
+        finally:
+            dnf.stop()
+
+        if run_on_loihi:
+            voltages = np.where(voltages == 128, 0, voltages)
+
+        return voltages
 
 
 if __name__ == '__main__':

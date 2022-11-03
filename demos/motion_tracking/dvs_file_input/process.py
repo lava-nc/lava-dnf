742997a0285f2607e57151c1a7282d1821f03164
SveaMeyer13 <svea.meyer@tum.de>
2022-11-03 08:23:53 -0700

    add comments

--- demos/motion_tracking/dvs_file_input/process.py
+++ demos/motion_tracking/dvs_file_input/process.py
@@ -17,6 +17,9 @@ from lava.magma.core.model.py.model import PyLoihiProcessModel
 
 
 class DVSFileInput(AbstractProcess):
+    """Process to read from .aedat4 file, downsample the frame in different modes
+    (max pooling, convolution, normal downsampling) and sends out the down sampled
+    event frame"""
     def __init__(self,
                  true_height: int,
                  true_width: int,
@@ -46,6 +49,7 @@ class DVSFileInput(AbstractProcess):
 @implements(proc=DVSFileInput, protocol=LoihiProtocol)
 @requires(CPU)
 class PyDVSFileInputPM(PyLoihiProcessModel):
+    # Declare port implementation
     event_frame_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, int)
 
     def __init__(self, proc_params):
@@ -69,13 +73,16 @@ class PyDVSFileInputPM(PyLoihiProcessModel):
         self._num_steps = proc_params["num_steps"]
 
     def run_spk(self):
+        # get next events from .aedat4 file
         events = self._event_stream.__next__()
         xs, ys, ps = events['x'], events['y'], events['polarity']
 
+        # write events to event frame
         event_frame = np.zeros(self._true_shape)
         event_frame[xs[ps == 0], ys[ps == 0]] = 1
         event_frame[xs[ps == 1], ys[ps == 1]] = 1
 
+        # downsample event frame
         if self._down_sample_mode == "down_sampling":
             event_frame_small = \
                 event_frame[::self._down_sample_factor,
